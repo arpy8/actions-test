3bc6bc7fb644026fe4f5efd31b2d6aceb5bced2a
Arpit <arpitsengar99@gmail.com>
2024-01-17 12:49:01 +0530

    asdasd

--- actions-test/main.py
+++ actions-test/main.py
@@ -1,1 +1,39 @@
-print("JELLSDIFHSBFSH")
+import os
+import pytz
+import datetime
+import requests
+
+API_URL = f'https://disdial.onrender.com/data'
+
+_chat_headers = {
+    'Authorization': os.environ["CHAT_TOKEN"],
+    'Content-Type': 'application/json',
+}
+
+def get_time():
+    current_time = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
+    return current_time.strftime('%Y-%m-%d %H:%M:%S %Z')
+
+def send_message_to_server(message):
+    data = {
+        'time': get_time(),
+        'username': "Github Action",
+        'message': message,
+    }
+
+    try:
+        response = requests.post(API_URL, json=data, headers=_chat_headers)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        return f"Request failed: {e}"
+
+
+def main():
+    try:
+        send_message_to_server("Hello World")
+        print("Message sent successfully")
+    except Exception as e:
+        print(e)
+        
+if __name__ == "__main__":
+    main()
